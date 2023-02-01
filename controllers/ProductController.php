096dbac7c13e3287c72758e91c907d6aac1fd43f
mjsimo99 <mohashark21@gmail.com>
2023-02-01 21:54:44 +0100

    fix back

--- controllers/ProductController.php
+++ controllers/ProductController.php
@@ -2,31 +2,82 @@
 require_once '../models/function.php';
 
 
+
+
+
+
+// class ProductController {
+//     public function index() {
+//         header('Access-Control-Allow-Origin:*');
+//         header('Content-type: application/json');
+//         header('Access-Control-Allow-Method: GET, POST, PUT, DELETE');
+//         header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Request-With');
+//         $requestMethod = $_SERVER['REQUEST_METHOD'];
+
+//         switch ($requestMethod) {
+//             case 'GET':
+//                 $productList = getProductList();
+//                 echo $productList;
+//                 break;
+//             case 'POST':
+//                 $product = json_decode(file_get_contents("php://input"), true);
+//                 $addedProduct = addProduct($product);
+//                 echo $addedProduct;
+//                 break;
+            
+//             case 'DELETE':
+//                 $productId = (int)$_GET['id'];
+//                 $deletedProduct = deleteProduct($productId);
+//                 echo $deletedProduct;
+//                 break;
+//             default:
+//                 $data = [
+//                     'status' => 405,
+//                     'message' => $requestMethod.'Method Not Allowed',
+//                 ];
+//                 header("HTTP/1.0 405 Method Not Allowed");
+//                 echo json_encode($data);
+//                 break;
+//         }
+//     }
+// }
 class ProductController {
     public function index() {
         header('Access-Control-Allow-Origin:*');
         header('Content-type: application/json');
-        header('Access-Control-Allow-Method: GET');
+        header('Access-Control-Allow-Method: GET, POST, PUT, DELETE');
         header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Request-With');
         $requestMethod = $_SERVER['REQUEST_METHOD'];
 
-        if ($requestMethod === "GET") {
-            $productList = getProductList();
-            echo $productList;
-        } else if ($requestMethod === "POST") {
-            $product = json_decode(file_get_contents("php://input"), true);
-            $addedProduct = addProduct($product);
-            echo $addedProduct;
-        } else {
-            $data = [
-                'status' => 405,
-                'message' => $requestMethod.'Method Not Allowed',
-            ];
-            header("HTTP/1.0 405 Method Not Allowed");
-            echo json_encode($data);
+        switch ($requestMethod) {
+            case 'GET':
+                $productList = getProductList();
+                echo $productList;
+                break;
+            case 'POST':
+                $product = json_decode(file_get_contents("php://input"), true);
+                $addedProduct = addProduct($product);
+                echo $addedProduct;
+                break;
+            case 'PUT':
+                $productId = (int)$_GET['id'];
+                $product = json_decode(file_get_contents("php://input"), true);
+                $updatedProduct = updateProduct($productId, $product);
+                echo $updatedProduct;
+                break;
+            case 'DELETE':
+                $productId = (int)$_GET['id'];
+                $deletedProduct = deleteProduct($productId);
+                echo $deletedProduct;
+                break;
+            default:
+                $data = [
+                    'status' => 405,
+                    'message' => $requestMethod.'Method Not Allowed',
+                ];
+                header("HTTP/1.0 405 Method Not Allowed");
+                echo json_encode($data);
+                break;
         }
     }
 }
-
-
-
--- models/function.php
+++ models/function.php
@@ -72,3 +72,62 @@ function addProduct($product) {
         die("Query Failed: " . $e->getMessage());
     }
 }
+
+
+function deleteProduct($id) {
+    global $conn;
+    try {
+        $stmt = $conn->prepare("DELETE FROM product WHERE id = :id");
+        $stmt->bindParam(':id', $id);
+        $stmt->execute();
+        $num_rows = $stmt->rowCount();
+        if ($num_rows > 0) {
+            $data = [
+                'status' => 200,
+                'message' => 'Product deleted successfully',
+                'id' => $id
+            ];
+            header("HTTP/1.0 200 Product deleted successfully");
+            return json_encode($data);
+        } else {
+            $data = [
+                'status' => 404,
+                'message' => 'Product not found'
+            ];
+            header("HTTP/1.0 404 Product not found");
+            return json_encode($data);
+        }
+    } catch (PDOException $e) {
+        die("Query Failed: " . $e->getMessage());
+    }
+}
+function updateProduct($id, $product) {
+    global $conn;
+    try {
+        $stmt = $conn->prepare("UPDATE product SET name=:name, email=:email, ref=:ref WHERE id=:id");
+        $stmt->bindParam(':id', $id);
+        $stmt->bindParam(':name', $product['name']);
+        $stmt->bindParam(':email', $product['email']);
+        $stmt->bindParam(':ref', $product['ref']);
+        $stmt->execute();
+        $num_rows = $stmt->rowCount();
+        if ($num_rows > 0) {
+            $data = [
+                'status' => 200,
+                'message' => 'Product updated successfully',
+                'product' => $product
+            ];
+            header("HTTP/1.0 200 Product updated successfully");
+            return json_encode($data);
+        } else {
+            $data = [
+                'status' => 500,
+                'message' => 'Failed to update product'
+            ];
+            header("HTTP/1.0 500 Failed to update product");
+            return json_encode($data);
+        }
+    } catch (PDOException $e) {
+        die("Query Failed: " . $e->getMessage());
+    }
+}
